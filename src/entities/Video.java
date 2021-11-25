23479dbb4e1916c1c233fc6db48701ab619bf912
Andrei Cherciu <72440310+AndreoOS@users.noreply.github.com>
2021-11-25 12:25:16 +0200

    Initial commit

---
+++ src/entities/Video.java
@@ -0,0 +1,11 @@
+package entities;
+
+import entertainment.Genre;
+
+import java.util.ArrayList;
+
+public class Video {
+    private String title;
+    private Integer releaseYear;
+    private ArrayList<Genre> genres;
+}
